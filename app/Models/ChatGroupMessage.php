982ea9ddc24dc1d4768d5dfd8eb70a1a5314ac1b
Than Toe Aung <thantoeaung@Thans-MacBook-Pro.local>
2023-04-10 23:06:30 +0630

    dd

--- app/Models/ChatGroupMessage.php
+++ app/Models/ChatGroupMessage.php
@@ -14,6 +14,6 @@ class ChatGroupMessage extends Model
     // }
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'id');
+        return $this->belongsTo(User::class, 'sender_id', 'id');
     }
 }
