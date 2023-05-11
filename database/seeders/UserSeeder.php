4448a399ee65f7654f61e244279bce504599f461
Htet-Yadanar <htetyatanaraung@gmail.com>
2023-05-11 20:55:05 +0630

    userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,6 +16,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         $user =User::create([
+            'id'=>1,
             'name'=>'King',
             'email' =>'king@gmail.com',
             'phone'=>'09422216317',
@@ -24,6 +25,7 @@ class UserSeeder extends Seeder
         $user->assignRole('King');
 
         $user =User::create([
+            'id'=>2,
             'name'=>'Queen',
             'email' =>'queen@gmail.com',
             'phone'=>'09250320376',
@@ -32,6 +34,7 @@ class UserSeeder extends Seeder
         $user->assignRole('Queen');
         
         $user =User::create([
+            'id'=>3,
             'name'=>'user',
             'email' =>'user@gmail.com',
             'phone'=>'0912345678',
@@ -40,6 +43,7 @@ class UserSeeder extends Seeder
         $user->assignRole('System_Admin');
 
         $user =User::create([
+            'id'=>4,
             'name'=>'admin',
             'email' =>'admin@gmail.com',
             'phone'=>'09123456788',
@@ -48,11 +52,39 @@ class UserSeeder extends Seeder
         $user->assignRole('Admin');
 
         $user =User::create([
+            'id'=>5,
             'name'=>'trainer',
             'email' =>'trainer@gmail.com',
             'phone'=>'09123456789',
             'password' => Hash::make('12345678'),
            ]);
         $user->assignRole('Trainer');
+
+        $user =User::create([
+            'id'=>6,
+            'name'=>'ktw',
+            'email' =>'ktw@gmail.com',
+            'phone'=>'09791867084',
+            'member_type' => 'Free',
+            'password' => Hash::make('12345678!'),
+           ]);
+
+        $user =User::create([
+            'id'=>8,
+            'name'=>'Default User',
+            'email' =>'defaultaccount@gmail.com',
+            'phone'=>'09100100100',
+            'member_type' => 'Free',
+            'password' => Hash::make('12345678!'),
+           ]);
+
+        $user =User::create([
+            'id'=>10,
+            'name'=>'hya',
+            'email' =>'hya@gmail.com',
+            'phone'=>'09752396518',
+            'member_type' => 'Free',
+            'password' => Hash::make('12345678'),
+           ]);
     }
 }
